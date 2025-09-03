b4f4e62bdaed1070f2f075dc513879bb9a85f046
iancapon <iancaponrusso@gmail.com>
2025-09-03 20:46:56 -0300

    Correccion sobre convenciones

--- Ejercicios/app/_layout.tsx
+++ Ejercicios/app/_layout.tsx
@@ -1,16 +1,21 @@
 import { Tabs } from "expo-router";
 
 export default function Layout() {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="EjercicioContador"
-        options={{ title: "Contador" }}
-      />
-      <Tabs.Screen
-        name="EjercicioTarjetas"
-        options={{ title: "Tarjetas" }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{ title: "Perfil" }}
+            />
+            <Tabs.Screen
+                name="contadores"
+                options={{ title: "Contador" }}
+
+            />
+            <Tabs.Screen
+                name="tarjetas"
+                options={{ title: "Tarjetas" }}
+            />
+        </Tabs>
+    );
 }
